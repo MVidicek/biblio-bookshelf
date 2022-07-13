6723eaad1c47c7e1972f0e3665fb91ff82296a6f
MVidicek <mvidicek@protonmail.com>
2022-07-13 18:12:40 +0200

    Fixed FOUC by donwgrading to next@12.1.5

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import { MantineProvider, ColorSchemeProvider } from '@mantine/core';
 import { useHotkeys, useLocalStorage } from '@mantine/hooks';
 import { NotificationsProvider } from '@mantine/notifications';
 import { RouteGuard } from '../components/RouteGuard';
 
-function MyApp({ Component, pageProps }) {
+export default function App(props) {
+  const { Component, pageProps } = props;
   const [colorScheme, setColorScheme] = useLocalStorage({
     key: 'mantine-color-scheme',
     defaultValue: 'light',
@@ -19,26 +21,33 @@ function MyApp({ Component, pageProps }) {
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        theme={{
-          colorScheme,
-          loader: 'oval',
-        }}
-        withGlobalStyles
-        withNormalizeCSS
+    <>
+      <Head>
+        <title>Biblio</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <NotificationsProvider position='top-center' limit={5}>
-          {/* Add Route Guard */}
+        <MantineProvider
+          theme={{
+            colorScheme,
+            loader: 'oval',
+          }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <NotificationsProvider position='top-center' limit={5}>
+            {/* Add Route Guard */}
 
-          {getLayout(<Component {...pageProps} />)}
-        </NotificationsProvider>
-      </MantineProvider>
-    </ColorSchemeProvider>
+            {getLayout(<Component {...pageProps} />)}
+          </NotificationsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
   );
 }
-
-export default MyApp;
