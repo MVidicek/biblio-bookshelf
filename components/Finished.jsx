89759b03376683b9ff5e0040ad4eb4acd0d7d748
MVidicek <mvidicek@protonmail.com>
2022-08-12 18:54:56 +0200

    Reading & finished components

--- components/Finished.jsx
+++ components/Finished.jsx
@@ -1,3 +1,37 @@
-export default function Finished() {
-  return <div>Finished</div>;
+import { BooksTable } from "./Tables/BooksTable";
+import fetchDocuments from "../utils/fetch-documents";
+import { useEffect, useState } from "react";
+import { Loader } from "@mantine/core";
+import NoBooksAdded from "./NoBooksAdded";
+
+export default function Finished({ setPage }) {
+  const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchDocuments("finished")
+      .then((books) => {
+        setBooks(books);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, [loading]);
+
+  if (loading)
+    return <Loader size="xl" color="teal" style={{ marginTop: "25%" }} />;
+
+  if (books.length === 0) return <NoBooksAdded setPage={setPage} />;
+
+  return (
+    <div>
+      <BooksTable
+        data={books}
+        setLoading={setLoading}
+        collection={"finished"}
+      />
+    </div>
+  );
 }
--- components/Reading.jsx
+++ components/Reading.jsx
@@ -1,3 +1,33 @@
-export default function Reading() {
-  return <div>Test</div>;
+import { BooksTable } from "./Tables/BooksTable";
+import fetchDocuments from "../utils/fetch-documents";
+import { useEffect, useState } from "react";
+import { Loader } from "@mantine/core";
+import NoBooksAdded from "./NoBooksAdded";
+
+export default function Reading({ setPage }) {
+  const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchDocuments("reading")
+      .then((books) => {
+        setBooks(books);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, [loading]);
+
+  if (loading)
+    return <Loader size="xl" color="teal" style={{ marginTop: "25%" }} />;
+
+  if (books.length === 0) return <NoBooksAdded setPage={setPage} />;
+
+  return (
+    <div>
+      <BooksTable data={books} setLoading={setLoading} collection={"reading"} />
+    </div>
+  );
 }
